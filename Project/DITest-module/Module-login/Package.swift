ee777fd34db5f415560342209304ddb8ec43e6ba
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2022-01-17 20:46:48 +0700

    refactor: change minimum deployment to version 12

--- Project/DITest-module/Module-login/Package.swift
+++ Project/DITest-module/Module-login/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Module-login",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
